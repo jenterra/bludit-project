311751c1c4c8bbc294a9bb02d19d485d71097a3e
Jenterra <heorhiiovechko105@outlook.com>
2025-12-18 02:26:41 -0400

    Category page made

--- bl-themes/bpers/php/section/content.php
+++ bl-themes/bpers/php/section/content.php
@@ -1,6 +1,8 @@
 <?php
 if ($WHERE_AM_I == 'page') {
 include(THEME_DIR_PHP.'router/page.php');
+} elseif ($WHERE_AM_I == 'category') {
+include(THEME_DIR_PHP.'router/category.php');
 } else {
 include(THEME_DIR_PHP.'router/home.php');
 }?>
